35097439052c2dec39482fc091ea4e840f6b5f08
Burger King <kuasipccv@gmail.com>
2023-02-18 16:45:49 +0800

    update Arregation of CPU Utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
 #include "processor.h"
+#include "linux_parser.h"
+
+using namespace LinuxParser;
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.9; }
+float Processor::Utilization() {
+    std::string line, key;
+    std::ifstream stream(kProcDirectory + kStatFilename);
+    std::getline(stream, line);
+    
+    std::istringstream iss(line);
+    std::string cpu_name;
+    int user, nice, sys, idle, iowait, irq, softirq, steal, guest, guest_nice;
+    iss >> cpu_name >> user >> nice >> sys >> idle >> iowait >> irq >> softirq >> steal >> guest >> guest_nice;
+    int idle_time = idle + iowait;
+    int total_time = user + nice + sys + idle_time + irq + softirq + steal;
+    float utilization = static_cast<float> (total_time - idle_time) / total_time;
+  
+    return utilization;
+}
